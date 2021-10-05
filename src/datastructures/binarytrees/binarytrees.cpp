5ba1701f1a481bce610dcbb6d778b7ec5876a26e
burntfalafel <wadhwae@tcd.ie>
2021-10-05 15:39:57 +0100

    [checkpoint] binarytrees insertion complete

--- src/datastructures/binarytrees/binarytrees.cpp
+++ src/datastructures/binarytrees/binarytrees.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
+#include <queue>
 
 #include "include/binarytress.hpp"
 
 /* initializer list */
 Node::Node(std::string n, int d) : name{n}, data{d}, right{NULL}, left{NULL} {}
 
+/* insert node based on data, first fills left and then right. No intermediate logic */
 void binarytree_insert(Node** head, std::string name, int data)
 {
   Node* tmp = new Node(name, data);
@@ -13,6 +15,39 @@ void binarytree_insert(Node** head, std::string name, int data)
     *head = tmp;
     return;
   }
+  std::queue<Node*> q;
+  q.push(*head);
+
+  while(!q.empty())
+  {
+    Node *q_tmp = q.front();
+    q.pop();
+
+    if(q_tmp->left != NULL)
+      q.push(q_tmp->left);
+    else
+    {
+      q_tmp->left = tmp;
+      return;
+    }
+    if(q_tmp->right != NULL)
+      q.push(q_tmp->right);
+    else
+    {
+      q_tmp->right = tmp;
+      return;
+    }
+  }
+}
+
+
+void binarytree_nuke(Node**head)
+{
+  std::queue<Node*> q;
+  q.push(*head);
+
+
+
 
 
 
